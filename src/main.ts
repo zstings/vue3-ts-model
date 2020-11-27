6ce8546dd0f07179929dfb23a6cbbf9fc987f3da
qinzhongting <41415769+qinzhongting@users.noreply.github.com>
2020-11-27 14:45:50 +0800

    Update main.ts
    移除 axios

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import { Button, Radio, Input, Tag, Menu, Dropdown, InputNumber, Empty, Form, message } from 'ant-design-vue'
 const app = createApp(App)
-app.config.globalProperties.$axios = axios
 app.config.globalProperties.$message = message
 app.use(Button).use(Radio).use(Input).use(Tag).use(Menu).use(Dropdown).use(InputNumber).use(Empty).use(Form)
 app.use(router).mount('#app')
